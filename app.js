3565a0735ff4ce8ac9c455cd8c4b14e678ec0a63
zdjgx <zjdgx20131030@163.com>
2015-01-11 23:47:49 +0800

    nodeDatabase

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+	app = express(),
+	sys = require('sys'),
+	path = require('path'),
+	formidable = require("formidable");
+	
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+console.log('dirName: ' + __dirname);
+express.static(path.join(__dirname, '/static'));
+
+app.get('/', function(req, res) {
+	res.render('login', {
+		title: '登陆|注册'
+	});
+});
+
+app.listen(8000, function() {
+	console.log('nodeDatabase started on port 8000...');
+});
